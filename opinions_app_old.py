10f14fd1e39905c34e5c5d8e657ec12c1afa19e0
Marat Garifulun <magmasters@gmail.com>
2025-10-15 18:58:37 +0700

    final_version

--- opinions_app_old.py
+++ opinions_app_old.py
@@ -1,8 +1,11 @@
+import csv
 from datetime import datetime
 # Импортируется функция для выбора случайного значения:
 from random import randrange
 
-from flask import Flask, flash, redirect, render_template, url_for
+import click
+from flask import Flask, abort, flash, redirect, render_template, url_for
+from flask_migrate import Migrate
 # Импортируем класс для работы с ORM:
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -17,6 +20,7 @@ app.config['SECRET_KEY'] = 'MY SECRET KEY'
 # Создаём экземпляр SQLAlchemy и в качестве параметра
 # передаём в него экземпляр приложения Flask:
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Opinion(db.Model):
@@ -32,6 +36,7 @@ class Opinion(db.Model):
     # Дата и время — текущее время,
     # по этому столбцу база данных будет проиндексирована:
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    added_by = db.Column(db.String(64))
 
 
 # Класс формы Opinion.
@@ -58,8 +63,8 @@ def index_view():
     quantity = Opinion.query.count()
     # Если мнений нет...
     if not quantity:
-        # ...то возвращается сообщение:
-        return 'В базе данных мнений о фильмах нет.'
+        # ... то пользователь увидит ошибку 500:
+        abort(500)
     # Иначе выбирается случайное число в диапазоне от 0 до quantity,
     offset_value = randrange(quantity)
     # и определяется случайный объект - берём первую запись, из получившегося:
@@ -108,6 +113,47 @@ def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     # ...и передаём его в шаблон (шаблон тот же, что и для главной страницы):
     return render_template('opinion.html', opinion=opinion)
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    # Ошибка 500 возникает в нештатных ситуациях на сервере.
+    # Например, провалилась валидация данных.
+    # В таких случаях можно откатить изменения, не зафиксированные в БД,
+    # чтобы в базу не записалось ничего лишнего.
+    db.session.rollback()
+    # Пользователю вернётся страница, сгенерированная на основе шаблона 500.html.
+    # Этого шаблона пока нет, но сейчас мы его тоже создадим.
+    # Пользователь получит и код HTTP-ответа 500.
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # При ошибке 404 в качестве ответа вернётся страница, созданная
+    # на основе шаблона 404.html, и код HTTP-ответа 404:
+    return render_template('404.html'), 404
+
+
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    # Открываем файл:
+    with open('opinions.csv', encoding='utf-8') as f:
+        # Создаём итерируемый объект, который отображает каждую строку
+        # в качестве словаря с ключами из шапки файла:
+        reader = csv.DictReader(f)
+        # Для подсчёта строк добавляем счётчик:
+        counter = 0
+        for row in reader:
+            # Распакованный словарь используем
+            # для создания экземпляра модели Opinion:
+            opinion = Opinion(**row)
+            # Добавляем объект в сессию и коммитим:
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}')
 
 
 if __name__ == '__main__':
